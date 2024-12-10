01587abe97fadd61f03aba9167034fe5bd0c4e12
usamihayato <usami.hayato11@gmail.com>
2024-12-10 21:27:21 +0900

    clean: delete not use method

--- python/development/quick/quick-api/src/app/core/config.py
+++ python/development/quick/quick-api/src/app/core/config.py
@@ -1,4 +1,3 @@
-import os
 import yaml
 
 def get_connection_config():
@@ -6,21 +5,5 @@ def get_connection_config():
     接続設定を取得する。
     """
     with open('input/config/connection_config.yml', 'r', encoding='utf-8') as f:
-        config = yaml.safe_load(f)
-    return config
-
-def get_data_config():
-    """
-    データ設定を取得する。
-    """
-    with open('input/config/data_config.yml', 'r', encoding='utf-8') as f:
-        config = yaml.safe_load(f)
-    return config
-
-def get_logging_config():
-    """
-    ログ設定を取得する。
-    """
-    with open('input/config/logging_config.yml', 'r') as f:
         config = yaml.safe_load(f)
     return config
